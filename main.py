748e3e7f6cb1fa8d34006532189a48a8d33290de
Mustwey <135557972+Mustwey@users.noreply.github.com>
2023-10-15 22:12:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import argparse
-from config import requirements
+from pathlib import Path
+import random
+import string
+import termcolor
+
+from config import requirements, settings
 requirements.install_requirements()
+
 from utils.banner import display_banner
 from core.obfuscator import Obfuscator
 
@@ -9,13 +15,33 @@ def main():
     parser.add_argument("file_path", type=str, help="Path to the batch file that needs to be obfuscated.")
     args = parser.parse_args()
 
+    if not Path(args.file_path).exists():
+        print("Operation failed with error code: E102")
+        return
+
     display_banner()
 
-    obfuscator = Obfuscator()
-    success, error_code = obfuscator.obfuscate(args.file_path, method="HexObfuscation")
-
-    if not success:
-        print(f"Operation failed with error code: {error_code}")
+    # Determine the output filename and path
+    file_name = Path(args.file_path).name
+    output_file_name = f"{settings.OUTPUT_PREFIX}{file_name}{settings.OUTPUT_SUFFIX}"
+    output_file_path = Path(settings.OUTPUT_PATH, output_file_name)
+    
+    # Create the output directory if it doesn't exist
+    Path(settings.OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
+    
+    for method in settings.OBFUSCATION_METHODS:
+        obfuscator = Obfuscator()
+        success, result_or_error = obfuscator.obfuscate(args.file_path, method=method['name'], **method['args'])
+        
+        if success:
+            out_hex = result_or_error
+            with open(output_file_path, 'wb') as f:
+                for i in out_hex:
+                    f.write(bytes.fromhex(i))
+                    
+            print(termcolor.colored(f'\n[ ✔ ] File Obfuscated Successfully as {output_file_name}', 'cyan'))
+        else:
+            print(f"Operation failed with error code: {result_or_error}")
 
 if __name__ == "__main__":
     try:
