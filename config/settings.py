31ac6f81fd4d1ab8630025069f9a6948ce07df65
Mustwey <135557972+Mustwey@users.noreply.github.com>
2023-10-15 22:10:40 -0400

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,14 @@
-# Configuration settings can be added here.
-# For example, default obfuscation method, logging level etc.
+# Define settings for obfuscation methods and output path
+
+# List of obfuscation methods to be used and their arguments
+OBFUSCATION_METHODS = [
+    {
+        "name": "HexObfuscation",
+        "args": {}
+    }
+]
+
+# Output settings
+OUTPUT_PATH = "C:\\Users\\alize\\Downloads\\batobfuscate-main\\obfuscated_files\\"
+OUTPUT_PREFIX = "obf_"
+OUTPUT_SUFFIX = ""
