64dae447ff5d9d62f7bb52e46ab5cb891bed5ee5
Mustwey <135557972+Mustwey@users.noreply.github.com>
2023-10-15 22:11:06 -0400

    Update obfuscator.py

--- core/obfuscator.py
+++ core/obfuscator.py
@@ -3,14 +3,13 @@ from obfuscation_methods import HexObfuscation
 class Obfuscator:
     def __init__(self):
         self.methods = {
-            "HexObfuscation": HexObfuscation,
-            # Additional methods can be added here
+            "HexObfuscation": HexObfuscation
         }
 
-    def obfuscate(self, file_path, method="HexObfuscation"):
+    def obfuscate(self, file_path, method="HexObfuscation", **kwargs):
         obfuscation_method = self.methods.get(method, None)
         if obfuscation_method is None:
             return False, "E101"
 
         obfuscator_instance = obfuscation_method()
-        return obfuscator_instance.obfuscate(file_path)
+        return obfuscator_instance.obfuscate(file_path, **kwargs)
