bdb7e1e09d1fec55d4a94b8eea7e87e36bb980c4
Mustwey <135557972+Mustwey@users.noreply.github.com>
2023-10-15 22:34:04 -0400

    Update HexObfuscation.py

--- obfuscation_methods/HexObfuscation.py
+++ obfuscation_methods/HexObfuscation.py
@@ -2,13 +2,13 @@ import random
 import string
 
 class HexObfuscation:
-    def obfuscate(self, file_path, **kwargs):
+    def obfuscate(self, file_content, **kwargs):
         try:
             out_hex = ["FF", "FE", "26", "63", "6C", "73", "0D", "0A", "FF", "FE", "0A", "0D"]
-            with open(file_path, 'rb') as f:
-                content = f.read()
-            out_hex.extend(['{:02X}'.format(b) for b in content])
-            return True, out_hex
+            out_hex.extend(['{:02X}'.format(b) for b in file_content])
+            out_bytes = bytes.fromhex(''.join(out_hex))
+            return True, out_bytes
         except Exception as e:
             print(f"An error occurred during obfuscation: {e}")
             return False, "E103"
+
